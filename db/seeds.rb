f17fcd5a9c996060fadb7aba620ba3517feae286
Giulia Laureti <giulia.laureti.be@gmail.com>
2023-11-20 16:15:56 +0100

    first branch

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
+require 'faker'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   doctors = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Doctor.destroy_all
+
+5.times do
+doctor = Doctor.new(
+  first_name: Faker::Name.first_name,
+  last_name: Faker::Name.last_name,
+  title: ['Dr', 'Prof'].sample,
+  available: Faker::Boolean.boolean,
+  specialty: ['Dermatologist', 'Pneumologist', 'Cardiologist', 'Dentist', 'GP'].sample,
+  experience: Faker::Number.between(from: 2, to: 6)
+)
+doctor.save!
+end
+puts 'Finished!'
