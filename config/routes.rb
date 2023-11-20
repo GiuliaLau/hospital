8db5d80f3d53a323d1006212e061a998a1703c29
Giulia Laureti <giulia.laureti.be@gmail.com>
2023-11-20 16:35:32 +0100

    post action

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root to: "doctors#index"
-  get "doctors", to: "doctors#index"
+  get "doctors", to: "doctors#index", as: :doctors
   get "doctors/new", to: "doctors#new", as: :new_doctor
+  post "doctors", to: "doctors#create"
   get "doctors/:id", to: "doctors#show", as: :doctor
 end
